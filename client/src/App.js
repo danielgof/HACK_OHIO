71de85df191b7d1b3e436d72177cc8c5a4b94580
danielgof <gofmandon@gmail.com>
2023-10-28 11:43:49 -0400

    pages templates

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,20 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import BottomBar from "./components/BottomBar/BottomBar.jsx";
 import TopBar from "./components/TopBar/TopBar.jsx";
 import "./App.css";
+import MainPage from "./pages/MainPage/MainPage.jsx";
+import InfoPage from "./pages/InfoPage/InfoPage.jsx";
+import MapPage from "./pages/InfoPage/InfoPage.jsx";
 
 function App() {
   return (
     <BrowserRouter>
-      <TopBar />
       <div className="App">
+        <TopBar />
+        <Routes>
+          <Route path="/" element={<MainPage />} />
+          <Route path="/info" element={<InfoPage />} />
+          <Route path="/map" element={<MapPage />} />
+        </Routes>
         <BottomBar />
       </div>
     </BrowserRouter>
--- client/src/pages/InfoPage/InfoPage.jsx
+++ client/src/pages/InfoPage/InfoPage.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import Form from "react-bootstrap/Form";
+import InputGroup from "react-bootstrap/InputGroup";
 
 const InfoPage = () => {
-  return <div>InfoPage</div>;
+  return (
+    <>
+      <br></br>
+      <br></br>
+      <br></br>
+      <InputGroup className="mb-3">
+        <Form.Control
+          placeholder="Recipient's username"
+          aria-label="Recipient's username"
+          aria-describedby="basic-addon2"
+        />
+        <InputGroup.Text id="basic-addon2">@example.com</InputGroup.Text>
+      </InputGroup>
+    </>
+  );
 };
 
 export default InfoPage;
