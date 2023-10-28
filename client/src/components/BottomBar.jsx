0cdb97b48e83e1ab4382dacd4ac3207c50de11eb
holmes301 <quintonrholmes30@gmail.com>
2023-10-28 11:00:16 -0400

    Added basic elements

---
+++ client/src/components/BottomBar.jsx
@@ -0,0 +1,29 @@
+import React, { useContext } from "react";
+import Container from "react-bootstrap/Container";
+import Navbar from "react-bootstrap/Navbar";
+import { Link } from "react-router-dom";
+
+const BottomBar = () => {
+    return (
+        <Navbar
+        expand = "lg"
+        bg={"light"}
+        data-bs-theme={"light"}
+        fixed="bottom">
+        <Container>
+            <Navbar.Brand
+            style={{color: "light"}}
+            as={Link}
+            to="/">
+            <span
+            style={{color: "light"}}
+            dangerouslySetInnerHTML={{__html: "&copy;" }}
+            />{" "}
+            TEST
+            </Navbar.Brand>
+        </Container>
+        </Navbar>
+    );
+};
+
+export default BottomBar;
