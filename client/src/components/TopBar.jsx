d7bcae3c8e7bad196fa462bb2baec9e55af7ee3d
holmes301 <quintonrholmes30@gmail.com>
2023-10-28 11:24:52 -0400

    image

--- client/src/components/TopBar.jsx
+++ client/src/components/TopBar.jsx
@@ -4,36 +4,39 @@ import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
 import { Link } from "react-router-dom";
+import logo from "../assets/foobarlogo.png";
 
 const TopBar = () => {
-    return (
-        <Navbar
-        bg={"light"}
-        data-bs-theme={"light"}
-        expand="lg"
-        className="bg-body-tertiary">
-        <Container>
-        <Nav.Link
-        style={{color: "light"}}
-        as={Link}
-        to="/maps">
-        Maps
+  return (
+    <Navbar
+      bg={"light"}
+      data-bs-theme={"light"}
+      expand="lg"
+      className="bg-body-tertiary"
+    >
+      <Container>
+        <Navbar.Brand as={Link} to="/">
+          <img
+            backgroundcolor="white"
+            src={"light"}
+            height={"light"}
+            width="54"
+            className="d-inline-block align-top"
+            alt="Logo"
+          />
+        </Navbar.Brand>
+        <Nav.Link style={{ color: "light" }} as={Link} to="/maps">
+          Maps
         </Nav.Link>
-        <Nav.Link
-        style={{color: "light"}}
-        as={Link}
-        to="/info">
-        Info
+        <Nav.Link style={{ color: "light" }} as={Link} to="/info">
+          Info
         </Nav.Link>
-        <Nav.Link
-        style={{color: "light"}}
-        as={Link}
-        to="/main">
-        Main
+        <Nav.Link style={{ color: "light" }} as={Link} to="/main">
+          Main
         </Nav.Link>
-        </Container>
-        </Navbar>
-    );
+      </Container>
+    </Navbar>
+  );
 };
 
 export default TopBar;
