d30b3d39a6013cc0740bb9426c7eb7f95c1e9606
danielgof <gofmandon@gmail.com>
2023-10-28 11:23:23 -0400

    templates

---
+++ client/src/components/TopBar/TopBar.jsx
@@ -0,0 +1,31 @@
+import { useContext } from "react";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import { Link } from "react-router-dom";
+
+const TopBar = () => {
+  return (
+    <Navbar
+      bg={"light"}
+      data-bs-theme={"light"}
+      expand="lg"
+      className="bg-body-tertiary"
+    >
+      <Container>
+        <Nav.Link style={{ color: "light" }} as={Link} to="/maps">
+          Maps
+        </Nav.Link>
+        <Nav.Link style={{ color: "light" }} as={Link} to="/info">
+          Info
+        </Nav.Link>
+        <Nav.Link style={{ color: "light" }} as={Link} to="/main">
+          Main
+        </Nav.Link>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default TopBar;
