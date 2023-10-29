16105552a781195e8c67866df03e003134cd56e5
holmes301 <quintonrholmes30@gmail.com>
2023-10-29 04:24:59 -0400

    added marker

--- client/src/pages/MapPage/MapPage.jsx
+++ client/src/pages/MapPage/MapPage.jsx
@@ -3,6 +3,8 @@ import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import { useNavigate } from "react-router-dom";
 import "leaflet/dist/leaflet.css";
 import "./MapPage.css";
+import L from "leaflet";
+import icon from "../../assets/position-marker.png";
 
 const MapPage = () => {
   const navigate = useNavigate();
@@ -14,6 +16,13 @@ const MapPage = () => {
       state: { id: Math.floor(lat) },
     });
   };
+
+  // Create a custom icon using the image URL
+  const customIcon = new L.Icon({
+    iconUrl: icon,
+    iconSize: [40, 40], // Width and height of the icon
+    iconAnchor: [20, 40], // Anchor point (center of the icon)
+  });
   return (
     <div style={{ backgroundColor: "#2D3032" }} className="raleway-font">
       <h1>Map</h1>
@@ -30,6 +39,7 @@ const MapPage = () => {
           <Marker
             position={[39.9979, -83.0087]}
             eventHandlers={{ click: handleMarkerClick }}
+            icon={customIcon}
           >
             <Popup>
               A pretty CSS3 popup. <br /> Easily customizable.
