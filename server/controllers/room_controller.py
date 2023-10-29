4037522d9adcf61efc976342616ce90d58e18be8
YURADEAD <90732098+YURADEAD@users.noreply.github.com>
2023-10-28 23:12:37 -0400

    Refactored building and room controllers and api's

--- server/controllers/room_controller.py
+++ server/controllers/room_controller.py
@@ -1,4 +1,15 @@
-
+from flask import Blueprint, request
+from create import *
+from models.room import *
+from models.building import *
 
 def bulilding_rooms(int: id):
     pass
+
+def get_rooms_building():
+    data = request.get_json(force=True)
+    query = session.query(Room).filter(Building.building_name == data["building_name"]).all()
+    res = []
+    for room in query:
+        res.append(room.to_json())
+    return res
