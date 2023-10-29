7d68fcadeec3c4af17fa4b3df9a0c391f6815784
holmes301 <quintonrholmes30@gmail.com>
2023-10-29 02:38:59 -0400

    added extra css elements

--- client/src/components/TopBar/TopBar.jsx
+++ client/src/components/TopBar/TopBar.jsx
@@ -3,7 +3,7 @@ import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import { Link } from "react-router-dom";
-import logo from "../../assets/foobarlogo.png";
+import logo from "../../assets/treasure-map.png";
 import "./TopBar.css";
 
 const TopBar = () => {
