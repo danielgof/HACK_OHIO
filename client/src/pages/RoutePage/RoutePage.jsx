c6727c589a1defd28bc352df2d891e9d84b2ecdb
holmes301 <quintonrholmes30@gmail.com>
2023-10-29 09:18:17 -0400

    fancy

--- client/src/pages/RoutePage/RoutePage.jsx
+++ client/src/pages/RoutePage/RoutePage.jsx
@@ -4,28 +4,31 @@ import Row from "react-bootstrap/Row";
 import { useLocation } from "react-router-dom";
 import placeholder from "../../assets/indoor-maps.jpg";
 import Col from "react-bootstrap/esm/Col";
+import "./RoutePage.css";
 
 const RoutePage = () => {
   const location = useLocation();
   return (
     <>
-      <br></br>
-      <h1>Navigation to {location.state.room_name}</h1>
-      <br></br>
-      <Container>
-        <Row>
-          <Col>
-            <img style={{ width: "50%" }} src={placeholder} alt="img" />
-          </Col>
-          <Col>
-            <ul>
-              <li>task 1</li>
-              <li>task 2</li>
-              <li>task 3</li>
-            </ul>
-          </Col>
-        </Row>
-      </Container>
+      <div className="raleway-font" style={{ color: "black" }}>
+        <br></br>
+        <h1>Navigation to {location.state.room_name}</h1>
+        <br></br>
+        <Container>
+          <Row>
+            <Col>
+              <img style={{ width: "50%" }} src={placeholder} alt="img" />
+            </Col>
+            <Col>
+              <ul>
+                <li>- Task 1 -</li>
+                <li>- Task 2 -</li>
+                <li>- Task 3 -</li>
+              </ul>
+            </Col>
+          </Row>
+        </Container>
+      </div>
     </>
   );
 };
