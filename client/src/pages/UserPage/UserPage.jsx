aacbc5cbf23710ac6d1484b8725dd3f09fdbd82c
holmes301 <quintonrholmes30@gmail.com>
2023-10-29 08:34:54 -0400

    frontend stuff

--- client/src/pages/UserPage/UserPage.jsx
+++ client/src/pages/UserPage/UserPage.jsx
@@ -1,12 +1,26 @@
 import React from "react";
 import { useLocation } from "react-router-dom";
+import icon from "../../assets/portrait.png";
+import Button from "react-bootstrap/Button";
+import "./UserPage.css";
 
 const UserPage = () => {
   const locatinon = useLocation();
   return (
     <>
-      {locatinon.state.id}
-      <div>user page</div>
+      <div className="raleway-font" style={{ color: "black" }}>
+        ID: {locatinon.state.id}
+        <br></br>
+        <img src={icon} style={{ width: "50px", height: "50px" }}></img>
+        <div>J. Smith</div>
+        <br></br>
+        <div>EMAIL: </div>
+        <br></br>
+        <Button variant="dark">My Buildings</Button>
+        <Button variant="dark" style={{ margin: "0 20px" }}>
+          My Rooms
+        </Button>
+      </div>
     </>
   );
 };
