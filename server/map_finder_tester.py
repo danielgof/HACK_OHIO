44c31ef519ce03c3ab447a4e9ebd1bf580526413
YURADEAD <90732098+YURADEAD@users.noreply.github.com>
2023-10-29 09:05:31 -0400

    Visual_Location now creates map on image

--- server/map_finder_tester.py
+++
@@ -1,4 +0,0 @@
-import visual_location
-
-maps = ['server/mapsbw/floor0bw.png', 'server/mapsbw/floor1bw.png', 'server/mapsbw/floor2bw.png', 'server/mapsbw/floor3bw.png']
-visual_location.visual_location("UNION MARKET", maps)
