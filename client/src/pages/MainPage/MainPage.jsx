b48c32b3dc2b9472f25cca85c0d939af2d95eaa6
holmes301 <quintonrholmes30@gmail.com>
2023-10-28 23:02:39 -0400

    image thingy

--- client/src/pages/MainPage/MainPage.jsx
+++ client/src/pages/MainPage/MainPage.jsx
@@ -1,11 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import Form from "react-bootstrap/Form";
 
 const MainPage = () => {
+  const [selectedFile, setSelectedFile] = useState(null);
+
+  const handleFileChange = (e) => {
+    const file = e.target.files[0];
+    setSelectedFile(file);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (!selectedFile) {
+      alert("Please select a file first.");
+      return;
+    }
+
+    const formData = new FormData();
+    formData.append("image", selectedFile);
+
+    fetch("/upload", {
+      method: "POST",
+      body: formData,
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        // Handle the server's response (e.g., display a success message)
+      })
+      .catch((error) => {
+        console.error("Error uploading the file:", error);
+      });
+  };
+
   return (
     <>
       <div align="center">
-        <h1>Take a picture of what around you</h1>
+        <h1>Take a picture of what's around you</h1>
         <br></br>
         <Form.Group controlId="formFileLg" className="mb-3">
           <Form.Control
@@ -13,8 +44,10 @@ const MainPage = () => {
             type="file"
             size="lg"
             accept="image/*"
+            onChange={handleFileChange}
           />
         </Form.Group>
+        <button onClick={handleSubmit}>Upload</button>
       </div>
     </>
   );
