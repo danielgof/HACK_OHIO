721506029f2d6ac6106ecb7239493627918db405
danielgof <gofmandon@gmail.com>
2023-10-29 08:06:31 -0400

    upd tables

--- server/models/building.py
+++ server/models/building.py
@@ -3,11 +3,12 @@ from sqlalchemy.orm import relationship
 from models.room import *
 from base import *
 
-"""Muildings"""
+"""Buildings"""
 class Building(Base):
     __tablename__ = "building"
     id = Column(Integer, primary_key=True)
     building_name = Column(String, nullable=False)
+    building_descr = Column(String, nullable=False)
     lat = Column(Float, nullable=False)
     lng = Column(Float, nullable=False)
     rooms = relationship("Room", secondary=building_rooms_association)
